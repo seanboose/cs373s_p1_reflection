1302851a545c88a62312fe53fd5266c691490383
Sean Boose <seanboose@gmail.com>
2016-01-25 17:06:33 -0600

    Initial commit.

---
+++ reflect/src/reflect/Reflect.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reflect;
+
+/**
+ *
+ * @author terriBoose
+ */
+public class Reflect {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String package_name = args[0];
+        String directory = args[1];
+        System.out.println("package name: " + package_name);
+        System.out.println("directory: " + directory);
+    }
+    
+}
