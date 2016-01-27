d3b3470e1dffe10e9d321f9dd1cc4656fa8a3e5b
Sean Boose <seanboose@gmail.com>
2016-01-27 14:42:10 -0600

    I think it's done! Messy, but done.

--- reflect/src/reflect/Main.java
+++ reflect/src/reflect/Main.java
@@ -10,9 +10,6 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.logging.Level;
@@ -33,16 +30,16 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        if(args.length < 2){
+            System.out.println("Required arguments: package_name, path/to/package");
+        }
         mPackage_name = args[0];
         mDirectory = args[1];
-//        System.out.println("package name: " + mPackage_name);
-//        System.out.println("directory: " + mDirectory);
-        
+
         printHeader();
         ArrayList<String> files = getFilesFromString(mDirectory);
 
-//        int classNum = 0;
-//        int memNum = 0;
         for(String file : files){
             Class<?> cls = getClassFromFilename(file);
             if(cls != null){
@@ -51,26 +48,27 @@ public class Main {
         }
     }
     
+    // Prints header information for output file
     private static void printHeader(){
         System.out.println(":- discontiguous bcClass/3, bcMember/7.");
         System.out.println(":- dynamic bcClass/3, bcMember/7.\n");
     }
     
+    // Creates a list of Strings from a directory File
     private static ArrayList<String> getFilesFromString(String directory){
         File dir = new File(directory);
         ArrayList<String> files = new ArrayList<String>(Arrays.asList(dir.list()));
         return files;
     }
     
+    // Returns a Class object from a filename
     // Returns null if class is not found
     private static Class<?> getClassFromFilename(String file) {
         Class<?> c = null;
         // Ignore non-.class files
         if(file.endsWith(".class")){
-            
             // Chop off the ".class" part of filename
             file = file.substring(0, file.length() - 6);
-            
             // Get the file!
             try {
                 c = Class.forName(mPackage_name + "." + file);
@@ -80,7 +78,8 @@ public class Main {
         }
         return c;
     }
-    
+    // Prints the contents of a Class object to match desired output for 
+    // assignment
     private static void printClassContents(Class<?> cls){
         int classNum = mClasses++;
         
@@ -115,22 +114,29 @@ public class Main {
         System.out.println("/*-------------*/");
     }
     
+    // Prints the contents of a Constructor object to match desired output for 
+    // assignment
     private static void printConstructor(Constructor con, int classNum){
         System.out.print("bcMember(");
         System.out.print("m" + mFields++ + ",");
         System.out.print("c" + classNum + ",");
         System.out.print("true,");
         System.out.print("false,");
-        System.out.print(con.getName() + ",");
-        System.out.print("<ARRAY>,");
-        System.out.print(con.getName() + "(");
+        System.out.print("'" + con.getName() + "',");
+        System.out.print("'',");
+        System.out.print("'" + formatConstructorName(con.getName()) + "(");
         printParameters(con.getParameterTypes());
         System.out.println(")').");
-        
-        
-
     }
     
+    // Simplifies fully qualified constructor class name (eg: "yparser.Main"
+    // becomes just "Main"
+    private static String formatConstructorName(String conName){
+        return conName.substring(conName.lastIndexOf('.')+1);
+    }
+    
+    // Takes the parameters of a method and prints properly formatted versions
+    // of them
     private static void printParameters(Class<?>[] params){
         if(params.length > 0){
             System.out.print(formatType(params[0].getName()));
@@ -141,6 +147,8 @@ public class Main {
         }
     }
     
+    // Prints the contents of a Field object to match desired output for 
+    // assignment
     private static void printField(Field field, int classNum){
         
         Modifier mod = new Modifier();
@@ -153,11 +161,11 @@ public class Main {
         System.out.print(mod.isStatic(mods) + ",");
         System.out.print("true,");
         System.out.print(type + ",");
-        System.out.print("<ARRAY>,");
-        System.out.println(field.getName() + ").");
-
+        System.out.print("'',");
+        System.out.println("'" + field.getName() + "').");
     }
     
+    // Prints the contents of a Method object to match desired output for assignment
     private static void printMethod(Method method, int classNum){
         Modifier mod = new Modifier();
         int mods = method.getModifiers();
@@ -166,20 +174,45 @@ public class Main {
         System.out.print("c" + classNum + ",");
         System.out.print(mod.isStatic(mods) + ",");
         System.out.print("false,");
-        System.out.print(formatType(method.getReturnType().getName()) + ",");
-        System.out.print("<ARRAY>,");
-        System.out.print(method.getName() + "(");
+        System.out.print("'" + formatType(method.getReturnType().getName()) + "',");
+        
+        System.out.print("'");
+        int arrayDepth = countArray(method.getReturnType().getName());
+        for(int i=0; i<arrayDepth; ++i){
+            System.out.print("[]");
+        }
+        
+        System.out.print("',");
+        System.out.print("'" + method.getName() + "(");
         printParameters(method.getParameterTypes());
         System.out.println(")').");
-
-        
     }
     
-    private static String formatType(String name){
-        String newName = name.replaceAll("java.lang.","").replaceAll(";","");
+    // Counts the depth of an array from a string containing a type name
+    // encoded in the Java Class "[[LObject" format
+    private static int countArray(String name){
+        int count = 0;
+        for(int i=0; i<name.length(); ++i){
+            if(name.charAt(i) == '[') ++count;
+        }
+        return count;
+    }
+    
+    // Given a String containing a type name, removes leading "java.lang." and
+    // reformats array indication from the java Class.geName() format .
+    // For example, "java.lang.[[LObject" to "Object[][]"
+    private static String formatType(String types){
         
-        // Remove array notation
-        newName = newName.replaceAll("(\\[)+L","");
-        return newName;
+        String newType = types.replaceAll("java.lang.","").replaceAll(";","");
+        int count = countArray(newType);
+        newType = newType.replaceAll("(\\[)+([ZBCLDFIJS])","");
+        StringBuilder temp = new StringBuilder(newType);
+        while(count > 0){
+            temp.append("[]");
+            --count;
+        }
+        newType = temp.toString();
+
+        return newType;
     }
 }
